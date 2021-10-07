b842f821d924ceb6db0e56db8085a00ce4995992
Rafaela <rafaela.o.barbosa@gmail.com>
2021-10-07 15:20:45 -0300

    [Feature Rafa : BlogPessoal - Atualização]

---
+++ src/main/java/org/generation/blog_pessoal/model/UserLogin.java
@@ -0,0 +1,47 @@
+package org.generation.blog_pessoal.model;
+
+public class UserLogin {
+	
+	private String nome;
+	
+	private String senha;
+	
+	private String token;
+	
+	private String usuario;
+
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+}
